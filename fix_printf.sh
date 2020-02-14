6b5e54a1f75c78fef093262373d4b8781ac9ad1f
Sagar Bijwe <sagar@espressif.com>
2020-02-14 12:14:11 +0530

    wifi: Change esp32s2beta dir to esp32s2 (5813c5b6)

--- fix_printf.sh
+++ fix_printf.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
-for dir in esp32 esp32s2beta; do
+for dir in esp32 esp32s2; do
     if [ -d "$dir" ]; then
         cd $dir
-        git status libphy.a | grep modified >/dev/null 2>&1
+        git status libphy.a | grep "[modified renamed]" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libphy.a fixed
             xtensa-esp32-elf-objcopy --redefine-sym ets_printf=phy_printf libphy.a
         fi
 
-        git status librtc.a | grep modified >/dev/null 2>&1
+        git status librtc.a | grep "[modified renamed]" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/librtc.a fixed
             xtensa-esp32-elf-objcopy --redefine-sym ets_printf=rtc_printf librtc.a
         fi
 
-        git status libsmartconfig.a | grep modified >/dev/null 2>&1
+        git status libsmartconfig.a | grep "[modified renamed]" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libsmartconfig.a fixed
             xtensa-esp32-elf-objcopy --redefine-sym printf=sc_printf libsmartconfig.a
         fi
 
-        git status libpp.a | grep modified >/dev/null 2>&1
+        git status libpp.a | grep "[modified renamed]" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libpp.a fixed
             xtensa-esp32-elf-objcopy --redefine-sym printf=pp_printf libpp.a
         fi
 
-        git status libnet80211.a | grep modified >/dev/null 2>&1
+        git status libnet80211.a | grep "[modified renamed]" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libnet80211.a fixed
             xtensa-esp32-elf-objcopy --redefine-sym printf=net80211_printf libnet80211.a
         fi
 
 
-        git status libcore.a | grep modified >/dev/null 2>&1
+        git status libcore.a | grep "[modified renamed]" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libcore.a fixed
             xtensa-esp32-elf-objcopy --redefine-sym printf=core_printf libcore.a
             xtensa-esp32-elf-objcopy --redefine-sym ets_printf=core_printf libcore.a
         fi
 
-        git status libcoexist.a | grep modified >/dev/null 2>&1
+        git status libcoexist.a | grep "[modified renamed]" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libcoexist.a fixed
             xtensa-esp32-elf-objcopy --redefine-sym ets_printf=coexist_printf libcoexist.a
