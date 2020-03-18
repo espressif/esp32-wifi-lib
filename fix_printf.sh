9cac9b5a60c05a1bf490aff5dc3685585e05db5a
Hrudaynath Dhabe <hrudaynath.dhabe@espressif.com>
2020-03-18 22:00:54 +0800

    ieee80211_ioctl: fix esp_wifi_sta_get_ap_info return values for cipher types (e5984a8f)

--- fix_printf.sh
+++ fix_printf.sh
@@ -1,14 +1,64 @@
 #!/bin/bash
-xtensa-esp32-elf-objcopy --redefine-sym ets_printf=phy_printf libphy.a 
-xtensa-esp32-elf-objcopy --redefine-sym ets_printf=rtc_printf librtc.a 
-xtensa-esp32-elf-objcopy --redefine-sym printf=wps_printf libwps.a 
-xtensa-esp32-elf-objcopy --redefine-sym ets_printf=wpa_printf libwpa.a 
-xtensa-esp32-elf-objcopy --redefine-sym printf=wpa2_printf libwpa2.a 
-xtensa-esp32-elf-objcopy --redefine-sym ets_printf=wpa2_printf libwpa2.a 
-xtensa-esp32-elf-objcopy --redefine-sym printf=sc_printf libsmartconfig.a 
-xtensa-esp32-elf-objcopy --redefine-sym printf=pp_printf libpp.a 
-xtensa-esp32-elf-objcopy --redefine-sym printf=net80211_printf libnet80211.a 
-xtensa-esp32-elf-objcopy --redefine-sym printf=core_printf libcore.a 
-xtensa-esp32-elf-objcopy --redefine-sym ets_printf=core_printf libcore.a 
-xtensa-esp32-elf-objcopy --redefine-sym ets_printf=coexist_printf libcoexist.a
-xtensa-esp32-elf-objcopy --redefine-sym printf=coexist_printf libcoexist.a
+dir=.
+git status libphy.a | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libphy.a fixed
+    xtensa-esp32-elf-objcopy --redefine-sym ets_printf=phy_printf libphy.a
+fi
+
+git status librtc.a | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/librtc.a fixed
+    xtensa-esp32-elf-objcopy --redefine-sym ets_printf=rtc_printf librtc.a
+fi
+
+git status libwps.a  | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libwps.a  fixed
+    xtensa-esp32-elf-objcopy --redefine-sym printf=wps_printf libwps.a
+fi
+
+git status libwpa.a  | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libwpa.a  fixed
+    xtensa-esp32-elf-objcopy --redefine-sym ets_printf=wpa_printf libwpa.a
+fi
+
+git status libwpa2.a  | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libwpa2.a  fixed
+    xtensa-esp32-elf-objcopy --redefine-sym printf=wpa2_printf libwpa2.a
+    xtensa-esp32-elf-objcopy --redefine-sym ets_printf=wpa2_printf libwpa2.a
+fi
+
+git status libsmartconfig.a | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libsmartconfig.a fixed
+    xtensa-esp32-elf-objcopy --redefine-sym printf=sc_printf libsmartconfig.a
+fi
+
+git status libpp.a | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libpp.a fixed
+    xtensa-esp32-elf-objcopy --redefine-sym printf=pp_printf libpp.a
+fi
+
+git status libnet80211.a | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libnet80211.a fixed
+    xtensa-esp32-elf-objcopy --redefine-sym printf=net80211_printf libnet80211.a
+fi
+
+git status libcore.a | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libcore.a fixed
+    xtensa-esp32-elf-objcopy --redefine-sym printf=core_printf libcore.a
+    xtensa-esp32-elf-objcopy --redefine-sym ets_printf=core_printf libcore.a
+fi
+
+git status libcoexist.a | grep modified >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $dir/libcoexist.a fixed
+    xtensa-esp32-elf-objcopy --redefine-sym ets_printf=coexist_printf libcoexist.a
+    xtensa-esp32-elf-objcopy --redefine-sym printf=coexist_printf libcoexist.a
+fi
