ab5deaa705985a972dbefa606b2dbac3e8df6329
Abhishek Prasad <abhishek.prasad@espressif.com>
2020-12-01 15:07:50 +0530

    ci: wlan feature tests addition for below test suites (e1e57aa6)
    1. PMF
    2. WPA3
    3. ASSOC
    4. MEMLEAK
    5. EAP
    6. SCAN
    7. AUTHMODE_DOWNGRADE
    8. ESPNOW
    9. EAPTLS

--- tools/ci/build_app.sh
+++ tools/ci/build_app.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This file is sourced in to the CI environment
 # in .gitlab-ci.yml file
-#
 
 set -x
 
@@ -12,13 +11,23 @@ die() {
 
 [ -z ${IDF_PATH} ] && die "IDF_PATH is not set."
 
-# Tool chain
-export PATH=/wifi_ci/xtensa-esp32-elf/bin:${PATH}
+# Set Env
+cd ${CI_PROJECT_DIR}/esp-idf
+./install.sh || { echo "Install Failure"; exit 1; }
+. ./export.sh || { echo "Export Failure"; exit 1; }
 
-# Sigma DUT
-echo Running make for espsigma dut app...
-cd ${CI_PROJECT_DIR}/espsigma/esp_sigma_dut
-make ${MAKEFLAGS} 
+# Set Env
+cd ${CI_PROJECT_DIR}/SSC
+
+# SSC Dut
+echo Running cmake for ssc app...
+if test -f "gen_misc_ng.sh"; then
+    ./gen_misc_ng.sh SSC
+fi
+
+if test -f "gen_misc_idf.sh"; then
+    ./gen_misc_idf.sh ESP32 SSC
+fi
 
 # Copying binaries to create build tar ball
 cd $CI_PROJECT_DIR
@@ -26,9 +35,10 @@ rm -rf ${board}-${CI_PIPELINE_ID}.debug
 mkdir -p ${board}-${CI_PIPELINE_ID}.debug/
 
 echo Creating ${board}-${VERSION} app binaries debug directory...
-cp ${CI_PROJECT_DIR}/espsigma/esp_sigma_dut/build/*.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
-cp ${CI_PROJECT_DIR}/espsigma/esp_sigma_dut/build/*.elf $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
-cp ${CI_PROJECT_DIR}/espsigma/esp_sigma_dut/build/bootloader/bootloader.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
+cp ${CI_PROJECT_DIR}/SSC/build/ssc.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
+cp ${CI_PROJECT_DIR}/SSC/build/ssc.elf $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
+cp ${CI_PROJECT_DIR}/SSC/build/bootloader/bootloader.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
+cp ${CI_PROJECT_DIR}/SSC/build/partition_table/partition-table.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
 
 # Creating tar file (debug)
 tar -zcvf $board-${CI_PIPELINE_ID}.debug.tar.gz $board-${CI_PIPELINE_ID}.debug || { echo "Create tar Status: Failure"; exit 1; }
