46d06df3ac605eca15ff72b8baff4d806b9f80f2
ChenJianxing <chenjianxing@espressif.com>
2020-12-03 21:15:05 +0800

    add esp32c3 wifi lib(e6877f70)

--- fix_printf.sh
+++ fix_printf.sh
@@ -57,3 +57,62 @@ for dir in esp32 esp32s2; do
         echo "$dir does not exist"
     fi
 done;
+
+for dir in esp32c3; do
+    if [ -d "$dir" ]; then
+        cd $dir
+        git status libphy.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/libphy.a fixed
+            riscv32-esp-elf-objcopy --redefine-sym ets_printf=phy_printf libphy.a
+        fi
+
+        git status librtc.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/librtc.a fixed
+            riscv32-esp-elf-objcopy --redefine-sym ets_printf=rtc_printf librtc.a
+        fi
+
+        git status libsmartconfig.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/libsmartconfig.a fixed
+            riscv32-esp-elf-objcopy --redefine-sym printf=sc_printf libsmartconfig.a
+        fi
+
+        git status libpp.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/libpp.a fixed
+            riscv32-esp-elf-objcopy --redefine-sym printf=pp_printf libpp.a
+        fi
+
+        git status libnet80211.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/libnet80211.a fixed
+            riscv32-esp-elf-objcopy --redefine-sym printf=net80211_printf libnet80211.a
+        fi
+
+        git status libmesh.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/libmesh.a fixed
+            riscv32-esp-elf-objcopy --redefine-sym printf=mesh_printf libmesh.a
+            riscv32-esp-elf-objcopy --redefine-sym ets_printf=mesh_printf libmesh.a
+        fi
+
+        git status libcore.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/libcore.a fixed
+            riscv32-esp-elf-objcopy --redefine-sym printf=core_printf libcore.a
+            riscv32-esp-elf-objcopy --redefine-sym ets_printf=core_printf libcore.a
+        fi
+
+        git status libcoexist.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/libcoexist.a fixed
+            riscv32-esp-elf-objcopy --redefine-sym ets_printf=coexist_printf libcoexist.a
+            riscv32-esp-elf-objcopy --redefine-sym printf=coexist_printf libcoexist.a
+        fi
+        cd ..
+    else
+        echo "$dir does not exist"
+    fi
+done;
