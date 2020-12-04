5f529fbb4f475b0f97bde7a77b9d5afaa8c2f4fe
Xia Xiaotian <xiaxiaotian@espressif.com>
2020-12-04 11:10:38 +0800

    support Wi-Fi modem sleep with MAC and baseband powering down(ccb95fec)

--- fix_printf.sh
+++ fix_printf.sh
@@ -1,115 +1,67 @@
 #!/bin/bash
-for dir in esp32 esp32s2; do
-    if [ -d "$dir" ]; then
-        cd $dir
-        git status libphy.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/libphy.a fixed
-            xtensa-esp32-elf-objcopy --redefine-sym ets_printf=phy_printf libphy.a
-        fi
-
-        git status librtc.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/librtc.a fixed
-            xtensa-esp32-elf-objcopy --redefine-sym ets_printf=rtc_printf librtc.a
-        fi
-
-        git status libsmartconfig.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/libsmartconfig.a fixed
-            xtensa-esp32-elf-objcopy --redefine-sym printf=sc_printf libsmartconfig.a
-        fi
-
-        git status libpp.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/libpp.a fixed
-            xtensa-esp32-elf-objcopy --redefine-sym printf=pp_printf libpp.a
-        fi
-
-        git status libnet80211.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/libnet80211.a fixed
-            xtensa-esp32-elf-objcopy --redefine-sym printf=net80211_printf libnet80211.a
-        fi
-
-        git status libmesh.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/libmesh.a fixed
-            xtensa-esp32-elf-objcopy --redefine-sym printf=mesh_printf libmesh.a
-            xtensa-esp32-elf-objcopy --redefine-sym ets_printf=mesh_printf libmesh.a
-        fi
-
-        git status libcore.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/libcore.a fixed
-            xtensa-esp32-elf-objcopy --redefine-sym printf=core_printf libcore.a
-            xtensa-esp32-elf-objcopy --redefine-sym ets_printf=core_printf libcore.a
-        fi
-
-        git status libcoexist.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/libcoexist.a fixed
-            xtensa-esp32-elf-objcopy --redefine-sym ets_printf=coexist_printf libcoexist.a
-            xtensa-esp32-elf-objcopy --redefine-sym printf=coexist_printf libcoexist.a
-        fi
-        cd ..
+for dir in esp32 esp32s2 esp32c3; do
+    if [ $dir = esp32 ]; then
+        TOOLCHAIN="xtensa-esp32-elf"
+    elif [ $dir = esp32s2 ]; then
+        TOOLCHAIN="xtensa-esp32s2-elf"
+    elif [ $dir = esp32c3 ]; then
+        TOOLCHAIN="riscv32-esp-elf"
     else
         echo "$dir does not exist"
     fi
-done;
-
-for dir in esp32c3; do
     if [ -d "$dir" ]; then
         cd $dir
         git status libphy.a | grep "modified" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libphy.a fixed
-            riscv32-esp-elf-objcopy --redefine-sym ets_printf=phy_printf libphy.a
+            $TOOLCHAIN-objcopy --redefine-sym ets_printf=phy_printf libphy.a
         fi
 
-        git status librtc.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/librtc.a fixed
-            riscv32-esp-elf-objcopy --redefine-sym ets_printf=rtc_printf librtc.a
+        if [ $dir = esp32 ] || [ $dir = esp32s2 ]; then
+            git status librtc.a | grep "modified" >/dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                echo $dir/librtc.a fixed
+                $TOOLCHAIN-objcopy --redefine-sym ets_printf=rtc_printf librtc.a
+            fi
         fi
 
         git status libsmartconfig.a | grep "modified" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libsmartconfig.a fixed
-            riscv32-esp-elf-objcopy --redefine-sym printf=sc_printf libsmartconfig.a
+            $TOOLCHAIN-objcopy --redefine-sym printf=sc_printf libsmartconfig.a
         fi
 
         git status libpp.a | grep "modified" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libpp.a fixed
-            riscv32-esp-elf-objcopy --redefine-sym printf=pp_printf libpp.a
+            $TOOLCHAIN-objcopy --redefine-sym printf=pp_printf libpp.a
         fi
 
         git status libnet80211.a | grep "modified" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libnet80211.a fixed
-            riscv32-esp-elf-objcopy --redefine-sym printf=net80211_printf libnet80211.a
+            $TOOLCHAIN-objcopy --redefine-sym printf=net80211_printf libnet80211.a
         fi
 
         git status libmesh.a | grep "modified" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libmesh.a fixed
-            riscv32-esp-elf-objcopy --redefine-sym printf=mesh_printf libmesh.a
-            riscv32-esp-elf-objcopy --redefine-sym ets_printf=mesh_printf libmesh.a
+            $TOOLCHAIN-objcopy --redefine-sym printf=mesh_printf libmesh.a
+            $TOOLCHAIN-objcopy --redefine-sym ets_printf=mesh_printf libmesh.a
         fi
 
         git status libcore.a | grep "modified" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libcore.a fixed
-            riscv32-esp-elf-objcopy --redefine-sym printf=core_printf libcore.a
-            riscv32-esp-elf-objcopy --redefine-sym ets_printf=core_printf libcore.a
+            $TOOLCHAIN-objcopy --redefine-sym printf=core_printf libcore.a
+            $TOOLCHAIN-objcopy --redefine-sym ets_printf=core_printf libcore.a
         fi
 
         git status libcoexist.a | grep "modified" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libcoexist.a fixed
-            riscv32-esp-elf-objcopy --redefine-sym ets_printf=coexist_printf libcoexist.a
-            riscv32-esp-elf-objcopy --redefine-sym printf=coexist_printf libcoexist.a
+            $TOOLCHAIN-objcopy --redefine-sym ets_printf=coexist_printf libcoexist.a
+            $TOOLCHAIN-objcopy --redefine-sym printf=coexist_printf libcoexist.a
         fi
         cd ..
     else
