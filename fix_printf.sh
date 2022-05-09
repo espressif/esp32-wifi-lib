05794b1c8898c9b43bd8c1b3418dea0f140ec303
Jack <jack@espressif.com>
2022-05-09 14:49:51 +0800

    bringup esp32c2 wifi, fully support connectionless traffic with ps and coex (c6eaf9cc)

--- fix_printf.sh
+++ fix_printf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for dir in esp32 esp32s2 esp32c3 esp32s3; do
+for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2; do
     if [ $dir = esp32 ]; then
         TOOLCHAIN="xtensa-esp32-elf"
     elif [ $dir = esp32s2 ]; then
         TOOLCHAIN="xtensa-esp32s2-elf"
-    elif [ $dir = esp32c3 ]; then
+    elif [ $dir = esp32c3 -o $dir = esp32c2 ]; then
         TOOLCHAIN="riscv32-esp-elf"
     elif [ $dir = esp32s3 ]; then
         TOOLCHAIN="xtensa-esp32s3-elf"
@@ -24,6 +24,7 @@ for dir in esp32 esp32s2 esp32c3 esp32s3; do
         if [ $? -eq 0 ]; then
             echo $dir/libpp.a fixed
             $TOOLCHAIN-objcopy --redefine-sym printf=pp_printf libpp.a
+            $TOOLCHAIN-objcopy --redefine-sym ets_printf=pp_printf libpp.a
         fi
 
         git status libnet80211.a | grep "modified" >/dev/null 2>&1
