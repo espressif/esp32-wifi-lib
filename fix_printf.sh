1eb9c86d74b7a0ea258e60bec5cec1cca2f6d248
lvshisheng <lvshisheng@espressif.com>
2022-12-29 12:56:33 +0800

    esp_wifi: support wifi6 features and add esp32c6 libs(34dba92c)

--- fix_printf.sh
+++ fix_printf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2; do
+for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6; do
     if [ $dir = esp32 ]; then
         TOOLCHAIN="xtensa-esp32-elf"
     elif [ $dir = esp32s2 ]; then
         TOOLCHAIN="xtensa-esp32s2-elf"
-    elif [ $dir = esp32c3 -o $dir = esp32c2 ]; then
+    elif [ $dir = esp32c3 -o $dir = esp32c2 -o $dir = esp32c6 ]; then
         TOOLCHAIN="riscv32-esp-elf"
     elif [ $dir = esp32s3 ]; then
         TOOLCHAIN="xtensa-esp32s3-elf"
