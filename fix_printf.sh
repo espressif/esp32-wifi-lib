e66a47c0ba36a6cf4edfe0dbf4d21588d07b374e
xuxiao <xuxiao@espressif.com>
2024-08-30 21:01:37 +0800

    feat(wifi): add wifi support for esp32c5 mp (e2b70f6e)

--- fix_printf.sh
+++ fix_printf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6 esp32_host esp32c5; do
+for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6 esp32_host esp32c5 esp32c61; do
     if [ $dir = esp32 ]; then
         TOOLCHAIN="xtensa-esp32-elf"
     elif [ $dir = esp32s2 ]; then
         TOOLCHAIN="xtensa-esp32s2-elf"
-    elif [ $dir = esp32c3 -o $dir = esp32c2 -o $dir = esp32c6 -o $dir = esp32_host -o $dir = esp32c5 ]; then
+    elif [ $dir = esp32c3 -o $dir = esp32c2 -o $dir = esp32c6 -o $dir = esp32_host -o $dir = esp32c5 -o $dir = esp32c61 ]; then
         TOOLCHAIN="riscv32-esp-elf"
     elif [ $dir = esp32s3 ]; then
         TOOLCHAIN="xtensa-esp32s3-elf"
