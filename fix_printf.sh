10dca7affa0ef6e50130a9bb8b2e1beb95bd4eb7
Jack <jack@espressif.com>
2025-07-31 10:28:51 +0800

    fix(wifi): add GCMP-128 cipher support for SoftAP mode (ee2fe1ca)

--- fix_printf.sh
+++ fix_printf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+COUNT=0
 for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6 esp32_host esp32c5 esp32c61; do
     if [ $dir = esp32 ]; then
         TOOLCHAIN="xtensa-esp32-elf"
@@ -15,55 +16,63 @@ for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6 esp32_host esp32c5 esp3
         chmod -x $dir/*;
         cd $dir
 
-        git status libsmartconfig.a | grep "modified\|new file" >/dev/null 2>&1
+        git status libsmartconfig.a | grep "modified\|修改\|new file\|新文件" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libsmartconfig.a fixed
+            ((COUNT++))
             $TOOLCHAIN-objcopy --redefine-sym printf=sc_printf libsmartconfig.a
         fi
 
-        git status libpp.a | grep "modified\|new file" >/dev/null 2>&1
+        git status libpp.a | grep "modified\|修改\|new file\|新文件" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libpp.a fixed
+            ((COUNT++))
             $TOOLCHAIN-objcopy --redefine-sym printf=pp_printf libpp.a
             $TOOLCHAIN-objcopy --redefine-sym ets_printf=pp_printf libpp.a
         fi
 
-        git status libnet80211.a | grep "modified\|new file" >/dev/null 2>&1
+        git status libnet80211.a | grep "modified\|修改\|new file\|新文件" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libnet80211.a fixed
+            ((COUNT++))
             $TOOLCHAIN-objcopy --redefine-sym printf=net80211_printf libnet80211.a
         fi
 
-        git status libtarget.a | grep "modified\|new file" >/dev/null 2>&1
+        git status libtarget.a | grep "modified\|修改\|new file\|新文件" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libtarget.a fixed
+            ((COUNT++))
             $TOOLCHAIN-objcopy --redefine-sym printf=target_printf libtarget.a
         fi
 
-        git status libmesh.a | grep "modified\|new file" >/dev/null 2>&1
+        git status libmesh.a | grep "modified\|修改\|new file\|新文件" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libmesh.a fixed
+            ((COUNT++))
             $TOOLCHAIN-objcopy --redefine-sym printf=mesh_printf libmesh.a
             $TOOLCHAIN-objcopy --redefine-sym ets_printf=mesh_printf libmesh.a
         fi
 
-        git status libcore.a | grep "modified\|new file" >/dev/null 2>&1
+        git status libcore.a | grep "modified\|修改\|new file\|新文件" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libcore.a fixed
+            ((COUNT++))
             $TOOLCHAIN-objcopy --redefine-sym printf=core_printf libcore.a
             $TOOLCHAIN-objcopy --redefine-sym ets_printf=core_printf libcore.a
         fi
 
-        git status libespnow.a | grep "modified\|new file" >/dev/null 2>&1
+        git status libespnow.a | grep "modified\|修改\|new file\|新文件" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libespnow.a fixed
+            ((COUNT++))
             $TOOLCHAIN-objcopy --redefine-sym ets_printf=espnow_printf libespnow.a
             $TOOLCHAIN-objcopy --redefine-sym printf=espnow_printf libespnow.a
         fi
 
-        git status libwapi.a | grep "modified\|new file" >/dev/null 2>&1
+        git status libwapi.a | grep "modified\|修改\|new file\|新文件" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo $dir/libwapi.a fixed
+            ((COUNT++))
             $TOOLCHAIN-objcopy --redefine-sym ets_printf=wapi_printf libwapi.a
             $TOOLCHAIN-objcopy --redefine-sym printf=wapi_printf libwapi.a
         fi
@@ -73,3 +82,4 @@ for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6 esp32_host esp32c5 esp3
         echo "$dir does not exist"
     fi
 done;
+echo $COUNT files fixed
