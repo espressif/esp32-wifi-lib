be8700b2cdca6d72a94713f6cd3809243c9bd8dc
yinqingzhao <yinqingzhao@espressif.com>
2026-04-15 17:09:40 +0800

    feat(wifi): add esp32s31 wifi support(5da3bdf9)

--- fix_printf.sh
+++ fix_printf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 COUNT=0
-for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6 esp32_host esp32c5 esp32c61; do
+for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6 esp32_host esp32c5 esp32c61 esp32s31; do
     if [ $dir = esp32 ]; then
         TOOLCHAIN="xtensa-esp32-elf"
     elif [ $dir = esp32s2 ]; then
         TOOLCHAIN="xtensa-esp32s2-elf"
-    elif [ $dir = esp32c3 -o $dir = esp32c2 -o $dir = esp32c6 -o $dir = esp32_host -o $dir = esp32c5 -o $dir = esp32c61 ]; then
+    elif [ $dir = esp32c3 -o $dir = esp32c2 -o $dir = esp32c6 -o $dir = esp32_host -o $dir = esp32c5 -o $dir = esp32c61 -o $dir = esp32s31 ]; then
         TOOLCHAIN="riscv32-esp-elf"
     elif [ $dir = esp32s3 ]; then
         TOOLCHAIN="xtensa-esp32s3-elf"
